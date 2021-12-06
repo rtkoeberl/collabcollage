67ab92cc8e4b5feea7433103768bd88c29b36cd7
Ross Koeberl <rtkoeberl@gmail.com>
2021-12-05 20:22:23 -0600

    Edited site info blurb

--- client/src/Components/PopUp.js
+++ client/src/Components/PopUp.js
@@ -17,15 +17,18 @@ export function PopUp({ showPopUp, togglePopUp }) {
         <button className="button" onClick={() => closePopUp()}>
           <p>✕</p>
         </button>
-        <h3>About CollabCollage</h3>
+        <h3>Welcome to CollabCollage!</h3>
         <p>
-          Welcome to CollabCollage, a website that allows you to quickly discover collaborations between multiple musical artists, using data from <a href="https://www.discogs.com" target="_blank" rel="noreferrer">Discogs</a>. To begin, use the search box to select two or more artists, then run your search to discover if and when their careers crossed paths. In addition, you can select the check box on a single artist in order to focus your search around their career.
+          This website allows you to quickly discover collaborations between musical artists, using data from <a href="https://www.discogs.com" target="_blank" rel="noreferrer">Discogs</a>.
         </p>
         <p>
-          The Discogs API rate limits queries to 100 release per second. In order to provide quicker results, CollabCollage backs up recent searches locally and copies larger discographies to a MongoDB database. Due to the nature of the data provided, CollabCollage cannot yet distinguish between performance and writing credits. As such, some results may be considered more "crossovers" than true collaborations. We still think they're neat.
+          To begin, use the search box to pick two or more artists, then run your search to discover if and when their careers crossed paths. To focus your search around a single artist's career, you can select the check box next to their name.
         </p>
         <p>
-          CollabCollage was developed by <a href="https://rosskoeberl.online" target="_blank" rel="noreferrer">Ross Koeberl</a>.
+          Due to the nature of the data provided, CollabCollage cannot yet distinguish between performance and writing credits. As such, some results may be considered more "crossovers" than true collaborations.
+        </p>
+        <p>
+          CollabCollage was developed by <a href="https://rosskoeberl.online" target="_blank" rel="noreferrer">Ross Koeberl</a>. You can read more about its development <a href="https://github.com/rtkoeberl/collabcollage" target="_blank" rel="noreferrer">here</a>.
         </p>
       </div>
     </div>
